f6f84f201c6d842a914f2c63a67521f5cc4ed2bb
Nishant Shekhar <s.nishant@ecomexpress.in>
2020-05-19 22:41:39 +0530

    Java Optional Basics And Method Usage

---
+++ FunctionalProgramming/src/main/java/optional/Main.java
@@ -0,0 +1,40 @@
+package optional;
+
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+        Optional<String> hello = Optional.ofNullable("hello");
+        System.out.println(hello.isEmpty());
+        System.out.println(hello.isPresent());
+
+        String world = hello
+                .map(String::toUpperCase)
+//                .or(() -> Optional.of("World")).get();
+                .orElse("world");
+        System.out.println(world);
+
+        world = hello.map(String::toUpperCase).orElseGet(() -> {
+            //extra computation
+            return "World";
+        });
+        System.out.println(world);
+
+        world = hello.map(String::toUpperCase).orElseThrow(IllegalStateException::new);
+        System.out.println(world);
+
+        Optional<String> optional = hello.map(String::toUpperCase).or(() -> Optional.of("Hello"));
+
+
+        hello.ifPresent(System.out::println);
+
+        hello.ifPresentOrElse(System.out::println, IllegalStateException::new);
+
+
+        Person person = new Person("Alex", "alex@email.com");
+        person.getEmail()
+                .ifPresentOrElse(System.out::println
+                        , () -> System.out.println("No Email"));
+    }
+}
+
---
+++ FunctionalProgramming/src/main/java/optional/Person.java
@@ -0,0 +1,23 @@
+package optional;
+
+import java.util.Optional;
+
+public class Person {
+
+    private String name;
+
+    private String email;
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Optional<String> getEmail() {
+        return Optional.ofNullable(email);
+    }
+}
