1209f547f102e5d241fce5858d795f3e4e156288
Nishant Shekhar <nishant.nishant.shekhar3@gmail.com>
2020-08-04 10:33:50 +0530

    Language Fundamentals Part 3 completed

---
+++ LanguageFundamentals/Part3/DataTypes.java
@@ -0,0 +1,8 @@
+class DataTypes{
+	public static void main(String... s){
+		boolean b = true;
+		// boolean b1 = True; compile time error, symbol not found. As True not suitable for any datatype then compiler treat it as a variable.
+		// boolean b2 = "True"; compile time error, incompatible types. String cannot be converted to boolean.
+		// char ch = null; compile time error, incompatible types. <null> cannot be converted to char.
+	}
+}
---
+++ LanguageFundamentals/Part3/README.md
@@ -0,0 +1,3 @@
+#Core Java with OCJP/SCJP: Language Fundamentals Part- 3 || Data Types part-2
+
+https://youtu.be/HYAosZptldA?list=PLd3UqWTnYXOmx_J1774ukG_rvrpyWczm0
