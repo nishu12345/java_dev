0f5cb127569762c0821ed16586ea8b3f548be4f5
Nishant <nishant.shekhar@coditas.com>
2021-03-02 08:11:19 +0530

    Program to reverse a sub-array.

--- src/main/java/week3/arrays_and_dynamic_arrays/ArrayReverse.java
+++ src/main/java/week3/arrays_and_dynamic_arrays/ArrayReverse.java
@@ -5,8 +5,17 @@ package week3.arrays_and_dynamic_arrays;
  *   Input  -> [1,2,3,4]
  *   Output -> [4,3,2,1]
  *   Time Complexity - O(n)
+ *
+ *   Reverse a sub-array from given array.
+ *   Input  -> [1,2,3,4,5,6,7,8,9]
+ *             start -> 2
+ *             end   -> 7
+ *   Output  -> [1,2,8,7,6,5,4,3,9]
+ *
  * */
 public class ArrayReverse {
+
+    /* Reverse the complete array */
     public static int[] reverseArray(int[] input) {
         int length = input.length;
         for (int i = 0; i < length / 2; i++) {
@@ -17,9 +26,26 @@ public class ArrayReverse {
         return input;
     }
 
+    /* Reverse a sub-array */
+    public static int[] reverseArray(int[] input, int start, int end) {
+        int limit = start + (((end + 1) - start)) / 2;
+        for (int i = start; i < limit; i++) {
+            input[i] = input[i] + input[end];
+            input[end] = input[i] - input[end];
+            input[i] = input[i] - input[end];
+            end--;
+        }
+        return input;
+    }
+
     public static void main(String[] args) {
-        int[] output = reverseArray(new int[]{1, 2, 3, 4});
-        for (int num : output) {
+        int[] reversedArray = reverseArray(new int[]{1, 2, 3, 4});
+        int[] reversedSubArray = reverseArray(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, 2, 7);
+        for (int num : reversedArray) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        for (int num : reversedSubArray) {
             System.out.print(num + " ");
         }
     }
