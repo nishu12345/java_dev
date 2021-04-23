5ba197e06e3ebdfb92ca77c10cb159a4d52b1a6e
Nishant <nishant.nishant.shekhar3@gmail.com>
2021-04-23 18:39:07 +0530

    Day 12 | Homework 1

--- src/main/java/week4/introduction_to_sorting/ElementsRemoval.java
+++ src/main/java/week4/introduction_to_sorting/ElementsRemoval.java
@@ -3,13 +3,27 @@ package week4.introduction_to_sorting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/*
+    Given an integer array A of size N. In one operation, you can remove any element from the array,
+    and the cost of this operation is the sum of all elements in the array present before this operation.
+    Find the minimum cost to remove all elements from the array.
+    Input->
+         A = [2, 1]
+    Output->
+        4
+    Explanation->
+        Given array A = [2, 1]
+        Remove 2 from the array => [1]. Cost of this operation is (2 + 1) = 3.
+        Remove 1 from the array => []. Cost of this operation is (1) = 1.
+        So, total cost is = 3 + 1 = 4.
+*/
 public class ElementsRemoval {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ElementsRemoval.class);
 
     public static void main(String[] args) {
-        int minCostRemoval = minCostRemoval(new int[]{8,0,10});
-        LOGGER.info("Min Cost of element removal for [2,3,1,4] {}", minCostRemoval);
+        int minCostRemoval = minCostRemoval(new int[]{8, 0, 10});
+        LOGGER.info("Min Cost of element removal for [8,0,10] {}", minCostRemoval);
     }
 
     public static int minCostRemoval(int[] A) {
