f7a1aea8e9575099eaca85c4adb2ba3a8f2e4a91
Nishant <nishant.nishant.shekhar3@gmail.com>
2021-06-08 10:03:40 +0530

    Day 26 | Assignment 1 - Better

--- src/main/java/week9/problem_solving_7/XorQueries.java
+++ src/main/java/week9/problem_solving_7/XorQueries.java
@@ -19,7 +19,8 @@ package week9.problem_solving_7;
 public class XorQueries {
 
     public static void main(String[] args) {
-        int[][] xorResults = getXorResults(new int[]{1, 0, 0, 0, 1}, new int[][]{{2, 4}, {1, 5}, {3, 5}});
+//        int[][] xorResults = getXorResults(new int[]{1, 0, 0, 0, 1}, new int[][]{{2, 4}, {1, 5}, {3, 5}});
+        int[][] xorResults = getXorResultsBetter(new int[]{1, 0, 0, 0, 1}, new int[][]{{2, 4}, {1, 5}, {3, 5}});
         for (int[] results : xorResults) {
             for (int num : results) {
                 System.out.print(num + " ");
@@ -42,6 +43,37 @@ public class XorQueries {
             }
             output[i] = new int[]{xor, xeroCount};
         }
+        return output;
+    }
+
+    public static int[][] getXorResultsBetter(int[] input, int[][] queries) {
+        int[][] output = new int[queries.length][2];
+//        calculate prefix count of 1's
+        for (int i = 1; i < input.length; ++i) {
+            if (input[i] == 1)
+                input[i] = input[i - 1] + 1;
+            else
+                input[i] = input[i - 1];
+        }
+
+        for (int i = 0; i < queries.length; ++i) {
+            int startIndex = queries[i][0] - 1;
+            int endIndex = queries[i][1] - 1;
+            int oneCount = input[endIndex];
+            int zeroCount = endIndex + 1 - oneCount;
+            if (startIndex != 0) {
+                int tempOneCount = input[startIndex - 1];
+                int tempZeroCount = startIndex - tempOneCount;
+                oneCount -= tempOneCount;
+                zeroCount -= tempZeroCount;
+            }
+            if (oneCount % 2 == 1 && zeroCount % 2 == 1)
+                output[i] = new int[]{1, zeroCount};
+            else if (oneCount % 2 == 1 && zeroCount % 2 == 0)
+                output[i] = new int[]{1, zeroCount};
+            else output[i] = new int[]{0, zeroCount};
+        }
+
         return output;
     }
 }
